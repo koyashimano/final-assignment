7257952974ff69bcddcb0aa6e5040610ee14aed5
koyashimano <qiaoyedaoye@gmail.com>
2024-05-09 10:18:05 +0900

    style: format provided codes

--- example.c
+++ example.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-  float *train_x = NULL;
-  unsigned char *train_y = NULL;
-  int train_count = -1;
+    float *train_x = NULL;
+    unsigned char *train_y = NULL;
+    int train_count = -1;
 
-  float *test_x = NULL;
-  unsigned char *test_y = NULL;
-  int test_count = -1;
+    float *test_x = NULL;
+    unsigned char *test_y = NULL;
+    int test_count = -1;
 
-  int width = -1;
-  int height = -1;
+    int width = -1;
+    int height = -1;
 
-  load_mnist(&train_x, &train_y, &train_count,
-             &test_x, &test_y, &test_count,
-             &width, &height);
+    load_mnist(&train_x, &train_y, &train_count,
+               &test_x, &test_y, &test_count,
+               &width, &height);
 
 /* 浮動小数点例外で停止することを確認するためのコード */
 #if 0
@@ -24,10 +24,10 @@ int main()
   volatile float z = x/y;
 #endif
 
-  // これ以降，３層NN の係数 A_784x10 および b_784x10 と，
-  // 訓練データ train_x + 784*i (i=0,...,train_count-1), train_y[0]～train_y[train_count-1],
-  // テストデータ test_x + 784*i (i=0,...,test_count-1), test_y[0]～test_y[test_count-1],
-  // を使用することができる．
+    // これ以降，３層NN の係数 A_784x10 および b_784x10 と，
+    // 訓練データ train_x + 784*i (i=0,...,train_count-1), train_y[0]～train_y[train_count-1],
+    // テストデータ test_x + 784*i (i=0,...,test_count-1), test_y[0]～test_y[test_count-1],
+    // を使用することができる．
 
-  return 0;
+    return 0;
 }
